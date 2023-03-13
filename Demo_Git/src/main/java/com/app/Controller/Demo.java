dd3151e307e886edcbdaf9cad1e0201e779a3a36
hai cao <haigb01@gmail.com>
2023-03-13 18:03:05 +0700

    Modify code

--- Demo_Git/src/main/java/com/app/Controller/Demo.java
+++ Demo_Git/src/main/java/com/app/Controller/Demo.java
@@ -3,7 +3,8 @@ package com.app.Controller;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Hello GitHub");
+		System.out.println("Hello world!");
+		System.out.println("Hello 2 nha");
 
 	}
 
